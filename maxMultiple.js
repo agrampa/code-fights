256349bd65a716d47232911b368479e6b4748bed
Allie Grampa <agrampa@yahoo.com>
2017-08-08 22:38:42 -0700

    added maxMultiple solution

---
+++ maxMultiple.js
@@ -0,0 +1,12 @@
+// given a divisor and a bound, find the largest integer N such that
+// - N is divisible by divisor
+// - N is less than or equal to bound
+// - N is greater than 0
+
+function maxMultiple(divisor, bound) {
+    for(let i = bound; i > 0; i--) {
+        if(i % divisor === 0) return i
+    }
+}
+
+maxMultiple(3, 10) // returns 9
