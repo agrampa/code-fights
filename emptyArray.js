26698ee93e0e4275b65621898026327e7c908357
Allie Grampa <agrampa@yahoo.com>
2017-08-14 14:04:05 -0700

    added solution for emptyArray

--- emptyArray.js
+++ emptyArray.js
@@ -7,17 +7,29 @@ let arrayList =  ['a','b','c','d','e','f'];
 // method 1
 arrayList = []
 
-// method 2
+// if you need to keep reference to the original...
 let arrayList = ['a','b','c','d','e','f']; // Created array
 let anotherArrayList = arrayList;  // Referenced arrayList by another variable
 arrayList = []; // Empty the array
 console.log(anotherArrayList); // Output ['a','b','c','d','e','f']
 
-// method 3
+// method 2
 arrayList.length = 0;
 
-// method 4
-let arrayList = ['a','b','c','d','e','f']; // Created array
-let anotherArrayList = arrayList;  // Referenced arrayList by another variable
+var arrayList = ['a','b','c','d','e','f']; // Created array
+var anotherArrayList = arrayList;  // Referenced arrayList by another variable
 arrayList.length = 0; // Empty the array by setting length to 0
 console.log(anotherArrayList); // Output []
+
+// method 3
+arrayList.splice(0, arrayList.length);
+
+var arrayList = ['a','b','c','d','e','f']; // Created array
+var anotherArrayList = arrayList;  // Referenced arrayList by another variable
+arrayList.splice(0, arrayList.length); // Empty the array by setting length to 0
+console.log(anotherArrayList); // Output []
+
+// method 4
+while(arrayList.length){
+  arrayList.pop();
+}
